97f34c879534356791970ba72c41f4cb7517966f
Nadifah <nadifah.rahmani@student.president.ac.id>
2025-08-13 16:24:24 +0700

    update august cv

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -36,7 +36,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="https://drive.google.com/file/d/1zRWxnp7gCK8DPixPRj3hOtbcQphXZq2D/view?usp=sharing"
+                href="https://drive.google.com/file/d/1-dvyIbnhI6I6nlzk8mQQT_U3GgaPJRrL/view?usp=sharing"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
